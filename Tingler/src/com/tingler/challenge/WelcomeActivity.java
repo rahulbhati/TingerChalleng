5469adc3228ce7195042e701ae286b3d83dfda4e
RahulBhati <Rahul@Rahul-PC>
2015-09-13 18:38:58 +0530

    First commit

---
+++ Tingler/src/com/tingler/challenge/WelcomeActivity.java
@@ -0,0 +1,24 @@
+package com.tingler.challenge;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class WelcomeActivity extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		checkLogin();
+		setContentView(R.layout.activity_welcome);
+	
+	}
+	public void checkLogin(){
+		
+	
+		
+		
+	}
+
+}
---
+++ Tingler/src/com/tingler/challenge/util/Profile.java
@@ -0,0 +1,14 @@
+package com.tingler.challenge.util;
+
+import android.content.SharedPreferences;
+
+public class Profile {
+	 public static final String MYProfile = "MyProfilePre" ;
+	 SharedPreferences sharedpreferences;
+	 
+	 
+	 
+	 
+	 
+	 
+}
