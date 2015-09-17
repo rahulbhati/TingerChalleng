c2370de81c019910fd83df22bf4534817f800e88
RahulBhati <Rahul@Rahul-PC>
2015-09-18 00:55:33 +0530

    Modified: Implement Navigation drawer rigt to left ...

--- Tingler/src/com/tingler/challenge/MainActivity.java
+++ Tingler/src/com/tingler/challenge/MainActivity.java
@@ -1,27 +1,90 @@
 package com.tingler.challenge;
 
+import com.tingler.challenge.fragment.About;
+import com.tingler.challenge.fragment.Dashboard;
+import com.tingler.challenge.fragment.Help;
 import com.tingler.challenge.fragment.NavigationDrawerFragment;
+import com.tingler.challenge.fragment.Notification;
+import com.tingler.challenge.fragment.Profile;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageView;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends FragmentActivity implements OnClickListener {
 	Toolbar toolbar;
     DrawerLayout drawerLayout;
+    ImageView imv_action_menu;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
        
         setContentView(R.layout.activity_main);
-        toolbar=(Toolbar)findViewById(R.id.toolBar);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        drawerLayout=(DrawerLayout)findViewById(R.id.drawerLayout);
-        NavigationDrawerFragment fragmentDrawer =(NavigationDrawerFragment)getFragmentManager().findFragmentById(R.id.drawerFragment);
-        fragmentDrawer.setUp(drawerLayout,toolbar,R.id.drawerFragment);
+        toolbar=(Toolbar)findViewById(R.id.toolbar);
+         init();
+       //  fragmentDrawer.setUp(drawerLayout,toolbar,R.id.drawerFragment);
+        
+    }
+public void init(){
+	  drawerLayout=(DrawerLayout)findViewById(R.id.drawerLayout);
+      NavigationDrawerFragment fragmentDrawer =(NavigationDrawerFragment)getFragmentManager().findFragmentById(R.id.drawerFragment);
+      imv_action_menu=(ImageView)findViewById(R.id.imv_action_menu);
+     
+      imv_action_menu.setOnClickListener(this);
+      
+}
+@Override
+public void onClick(View v) {
+	// TODO Auto-generated method stub
+	 drawerLayout.openDrawer(Gravity.END);
+	 
+}
+public void displayView(int position){
+	Fragment fragment = null;
+    switch (position) {
+    case 0:
+        fragment = new Profile();
+        break;
+    case 1:
+        fragment = new Dashboard(this);
+        break;
+    case 2:
+        fragment = new Notification();
+        break;
+    case 3:
+        fragment = new Help();
+        break;
+    case 4:
+        fragment = new About();
+        break;
+  
+    default:
+        break;
     }
 
+	 if (fragment != null) {
+           FragmentManager fragmentManager = getFragmentManager();
+           fragmentManager.beginTransaction()
+                   .replace(R.id.frame_container, fragment).commit();
+
+           // update selected item and title, then close the drawer
+         //  mDrawerList.setItemChecked(position, true);
+         //  mDrawerList.setSelection(position);
+        //   setTitle(navMenuTitles[position]);
+           drawerLayout.closeDrawers();
+       } else {
+           // error in creating fragment
+           Log.e("MainActivity", "Error in creating fragment");
+       }
+}
   
 }
--- Tingler/src/com/tingler/challenge/fragment/NavigationDrawerFragment.java
+++ Tingler/src/com/tingler/challenge/fragment/NavigationDrawerFragment.java
@@ -1,91 +1,146 @@
 package com.tingler.challenge.fragment;
 
-
-import java.util.ArrayList;
-import java.util.List;
-
 import android.app.Fragment;
+import android.app.FragmentManager;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.tingler.challenge.MainActivity;
 import com.tingler.challenge.R;
 import com.tingler.challenge.adapter.OptionAdapter;
-import com.tingler.challenge.util.OptionsData;
-
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class NavigationDrawerFragment extends Fragment {
+public class NavigationDrawerFragment extends Fragment implements OnClickListener {
 
-    Toolbar toolbar;
-    DrawerLayout drawerLayout;
-    private View viewContainer;
-    ActionBarDrawerToggle actionBarDrawerToggle;
-    RecyclerView drawerItems;
-    OptionAdapter itemAdapter;
+	Toolbar toolbar;
+	DrawerLayout drawerLayout;
+	private View viewContainer;
+	ActionBarDrawerToggle actionBarDrawerToggle;
 
-    public NavigationDrawerFragment() {
-        // Required empty public constructor
-    }
+	OptionAdapter itemAdapter;
+	TextView txt_profile, txt_dashboard, txt_notification, txt_helpcenter,
+			txt_aboutus, txt_update, txt_signout;
+	ImageView etxt_setting;
 
-    public void setUp(DrawerLayout drawerLayout, final Toolbar toolbar, int id) {
-        viewContainer = getActivity().findViewById(id);
-        this.toolbar = toolbar;
-        this.drawerLayout = drawerLayout;
-        actionBarDrawerToggle = new ActionBarDrawerToggle(getActivity(), this.drawerLayout, toolbar, R.string.open, R.string.close) {
-            @Override
-            public void onDrawerClosed(View drawerView) {
-                super.onDrawerClosed(drawerView);
-                getActivity().invalidateOptionsMenu();
-            }
+	public NavigationDrawerFragment() {
+		// Required empty public constructor
+	}
 
-            @Override
-            public void onDrawerOpened(View drawerView) {
-                super.onDrawerOpened(drawerView);
-                getActivity().invalidateOptionsMenu();
-            }
+	public void setUp(DrawerLayout drawerLayout, final Toolbar toolbar, int id) {
+		viewContainer = getActivity().findViewById(id);
+		this.toolbar = toolbar;
+		this.drawerLayout = drawerLayout;
+		actionBarDrawerToggle = new ActionBarDrawerToggle(getActivity(),
+				this.drawerLayout, toolbar, R.string.open, R.string.close) {
+			@Override
+			public void onDrawerClosed(View drawerView) {
+				super.onDrawerClosed(drawerView);
+				getActivity().invalidateOptionsMenu();
+			}
 
-            @Override
-            public void onDrawerSlide(View drawerView, float slideOffset) {
+			@Override
+			public void onDrawerOpened(View drawerView) {
+				super.onDrawerOpened(drawerView);
+				getActivity().invalidateOptionsMenu();
+			}
 
-            }
-        };
-        drawerLayout.post(new Runnable() {
-            @Override
-            public void run() {
-                actionBarDrawerToggle.syncState();
-            }
-        });
-        drawerLayout.setDrawerListener(actionBarDrawerToggle);
-    }
+			@Override
+			public void onDrawerSlide(View drawerView, float slideOffset) {
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragmen_drawer, container, false);
-        drawerItems = (RecyclerView) view.findViewById(R.id.drawerItems);
-        List<OptionsData> itemDatas = new ArrayList<OptionsData>();
-        for (int i = 0; i < 8; i++) {
-            OptionsData itemData = new OptionsData();
-            itemData.option = "Item " + i;
-            itemDatas.add(itemData);
-        }
+			}
+		};
+		drawerLayout.post(new Runnable() {
+			@Override
+			public void run() {
+				actionBarDrawerToggle.syncState();
+			}
+		});
+		drawerLayout.setDrawerListener(actionBarDrawerToggle);
+	}
 
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
 
-        itemAdapter = new OptionAdapter(getActivity(), itemDatas);
-        drawerItems.setAdapter(itemAdapter);
-        drawerItems.setLayoutManager(new LinearLayoutManager(getActivity()));
-        return view;
-    }
+		View view = inflater.inflate(R.layout.fragmen_drawer, container, false);
+	//	init(view);
+		return view;
+	}
 
+	public void init(View view) {
+		txt_profile=(TextView)view.findViewById(R.id.txt_profile);
+		txt_dashboard=(TextView)view.findViewById(R.id.txt_dashboard);
+		txt_notification=(TextView)view.findViewById(R.id.txt_notification);
+		txt_helpcenter=(TextView)view.findViewById(R.id.txt_helpcenter);
+		txt_aboutus=(TextView)view.findViewById(R.id.txt_aboutus);
+		txt_update=(TextView)view.findViewById(R.id.txt_update);
+		txt_signout=(TextView)view.findViewById(R.id.txt_signout);
+		etxt_setting=(ImageView)view.findViewById(R.id.etxt_setting);
+		
+		txt_profile.setOnClickListener(this);
+		txt_dashboard.setOnClickListener(this);
+		txt_notification.setOnClickListener(this);
+		txt_helpcenter.setOnClickListener(this);
+		txt_aboutus.setOnClickListener(this);
+		txt_update.setOnClickListener(this);
+		txt_signout.setOnClickListener(this);
+		etxt_setting.setOnClickListener(this);
+	}
 
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+	//	Fragment fragment = null;
+		MainActivity activity=new MainActivity();
+		if(v.getId()==R.id.txt_profile){
+			
+		
+			activity.displayView(0);
+			 
+	//		 fragment = new Profile();
+		}else if(v.getId()==R.id.txt_dashboard){
+			activity.displayView(1);
+	//		 fragment = new Dashboard(getActivity());
+		}else if(v.getId()==R.id.txt_notification){
+			activity.displayView(2);
+	//		 fragment = new Notification();
+		}else if(v.getId()==R.id.txt_helpcenter){
+			activity.displayView(3);
+	//		 fragment = new Help();
+		}else if(v.getId()==R.id.txt_aboutus){
+			activity.displayView(4);
+	//		 fragment = new About();
+		}else if(v.getId()==R.id.txt_update){
+		//	 fragment = new Upd
+		}else if(v.getId()==R.id.txt_signout){
+			// fragment = new HomeFragment();
+		}else if(v.getId()==R.id.etxt_setting){
+			// fragment = new HomeFragment();
+		}
+	//	 if (fragment != null) {
+	//            FragmentManager fragmentManager = getFragmentManager();
+	 //           fragmentManager.beginTransaction()
+	//                    .replace(R.id.frame_container, fragment).commit();
+	 
+	            // update selected item and title, then close the drawer
+	          //  mDrawerList.setItemChecked(position, true);
+	          //  mDrawerList.setSelection(position);
+	         //   setTitle(navMenuTitles[position]);
+	         //   mDrawerLayout.closeDrawer(mDrawerList);
+	  //      } else {
+	            // error in creating fragment
+	  //          Log.e("MainActivity", "Error in creating fragment");
+	  //      }
+	}
 }
