1c75b4b86e91d2090fdb2f56d7a188e6d4d879be
RahulBhati <Rahul@Rahul-PC>
2015-09-20 17:18:30 +0530

    Modified: Improve profile fragment page design... with circle image and progress bar ...

--- Tingler/src/com/tingler/challenge/fragment/Profile.java
+++ Tingler/src/com/tingler/challenge/fragment/Profile.java
@@ -4,18 +4,61 @@ import com.tingler.challenge.R;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 
-public class Profile  extends Fragment{
+public class Profile extends Fragment {
+	ProgressBar progressbar_profileLevel;
+	int pStatus = 0;
+	private Handler handler = new Handler();
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
-		View profileView=inflater.inflate(R.layout.fragment_profile,container,false);
+		View profileView = inflater.inflate(R.layout.fragment_profile,
+				container, false);
+		init(profileView);
 		return profileView;
 	}
 
+	public void init(View view) {
+		progressbar_profileLevel = (ProgressBar) view
+				.findViewById(R.id.progressbar_profileLevel);
+		progressBarLevel();
+	}
+	
+	public void progressBarLevel(){
+		new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				while (pStatus <= 100) {
+
+					handler.post(new Runnable() {
+
+						@Override
+						public void run() {
+							// TODO Auto-generated method stub
+							progressbar_profileLevel.setProgress(pStatus);
+							//progressBar1.setSecondaryProgress(pStatus + 5);
+							
+						}
+					});
+					try {
+						// Sleep for 200 milliseconds.
+						// Just to display the progress slowly
+						Thread.sleep(200);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+					pStatus++;
+				}
+			}
+		}).start();
+	}
 }
