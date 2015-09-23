70a1837f02c29f36977d0aaada61c102b958e2a3
RahulBhati <Rahul@Rahul-PC>
2015-09-23 08:57:03 +0530

    Modified: Desing final challenge price page...

--- Tingler/src/com/tingler/challenge/fragment/createchallenge/Price.java
+++ Tingler/src/com/tingler/challenge/fragment/createchallenge/Price.java
@@ -9,9 +9,11 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class Price extends Fragment implements OnClickListener{
 	Button btn_submit;
+	EditText etxt_price_type,etxt_coins;
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
@@ -24,11 +26,14 @@ public class Price extends Fragment implements OnClickListener{
 	{
 		
 		btn_submit=(Button)view.findViewById(R.id.btn_submit);
+		etxt_price_type=(EditText)view.findViewById(R.id.etxt_price_type);
+		etxt_coins=(EditText)view.findViewById(R.id.etxt_coins);
 		btn_submit.setOnClickListener(this);
 	}
 	@Override
 	public void onClick(View v) {
 		// TODO Auto-generated method stub
-		
+		String price=etxt_price_type.getText().toString();
+		String coins=etxt_coins.getText().toString();
 	}
 }
