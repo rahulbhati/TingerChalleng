693db233b15fdf8d390f732f7ee5ba1efdd77243
RahulBhati <Rahul@Rahul-PC>
2015-09-23 09:26:28 +0530

    Modified: Create challenge detail page...desing

--- Tingler/src/com/tingler/challenge/fragment/createchallenge/SetterGetter.java
+++ Tingler/src/com/tingler/challenge/fragment/createchallenge/SetterGetter.java
@@ -7,6 +7,51 @@ import com.tingler.challenge.util.ContactItem;
 public class SetterGetter {
 public static ArrayList<ContactItem> challengeeTempArrayList=new ArrayList<ContactItem>();
 public static ArrayList<ContactItem> witnessTempArrayList=new ArrayList<ContactItem>();
+
+public static String challengeTitle=null;
+public static String challengeDescription=null;
+public static String challengeDays=null,challengeHours=null,challengeMinutes=null;
+
+public static String getChallengeTitle() {
+	return challengeTitle;
+}
+
+public static String getChallengeDescription() {
+	return challengeDescription;
+}
+
+public static String getChallengeDays() {
+	return challengeDays;
+}
+
+public static String getChallengeHours() {
+	return challengeHours;
+}
+
+public static String getChallengeMinutes() {
+	return challengeMinutes;
+}
+
+public static void setChallengeTitle(String challengeTitle) {
+	SetterGetter.challengeTitle = challengeTitle;
+}
+
+public static void setChallengeDescription(String challengeDescription) {
+	SetterGetter.challengeDescription = challengeDescription;
+}
+
+public static void setChallengeDays(String challengeDays) {
+	SetterGetter.challengeDays = challengeDays;
+}
+
+public static void setChallengeHours(String challengeHours) {
+	SetterGetter.challengeHours = challengeHours;
+}
+
+public static void setChallengeMinutes(String challengeMinutes) {
+	SetterGetter.challengeMinutes = challengeMinutes;
+}
+
 public static ArrayList<ContactItem> getChallengeeTempArrayList() {
 	return challengeeTempArrayList;
 }
